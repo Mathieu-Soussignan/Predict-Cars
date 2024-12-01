d2991800182d6387da79a7647f556bc2c833373d
Mathieu Soussignan <contact.mathieu.soussignan@gmail.com>
2024-12-01 13:51:13 +0100

    Update crud.py

--- API/database.py
+++ API/database.py
@@ -1,10 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "sqlite:///./voitures_aramisauto.db"
+# Récupérer l'URL de la base de données depuis les variables d'environnement
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./voitures_aramisauto.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
